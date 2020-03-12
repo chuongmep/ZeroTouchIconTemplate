75537d389c024570ea2e1f4247c70d041d69866c
chuong9x <31106432+chuong9x@users.noreply.github.com>
2020-03-12 11:44:23 +0700

    Release

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ZeroTouchIcon")]
+[assembly: AssemblyDescription("http://chuongmep.com/")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://chuongmep.com/")]
+[assembly: AssemblyProduct("ZeroTouchIcon.Properties")]
+[assembly: AssemblyCopyright("2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("955dbc8d-5d26-47e1-81b8-6a5d6e3deea8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
